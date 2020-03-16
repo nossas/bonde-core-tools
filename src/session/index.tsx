cde6e9b005ae2c8feaa4950c838ab5e9b71e184e
Igor Santos <igr.exe@gmail.com>
2020-03-16 13:55:01 -0300

    chore: test export hooks to graphql context

--- src/session/index.tsx
+++ src/session/index.tsx
@@ -1,3 +1,4 @@
 export { default as SessionProvider } from './SessionProvider';
 export { SessionHOC, useSession } from './SessionProvider';
 export { SessionPage } from './components/PageLayout';
+export { useQuery, useMutation } from '@apollo/react-hooks';
