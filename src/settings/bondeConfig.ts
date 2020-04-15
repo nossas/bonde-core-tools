536d63fd1e6a06767c666cfb9b08fa16918871d3
Igor Santos <igr.exe@gmail.com>
2020-04-15 16:34:58 -0300

    chore(settings): update chatbot urls

--- src/settings/bondeConfig.ts
+++ src/settings/bondeConfig.ts
@@ -2,9 +2,9 @@ export default {
   development: {
     // Modules URLs
     accounts: 'http://accounts.bonde.devel:5000/login',
-    settings: 'http://admin-canary.bonde.devel:5001/community/settings',
+    settings: 'http://admin-canary.bonde.devel:5001/community',
     mobilization: 'http://app.bonde.devel:5002/mobilizations',
-    chatbot: 'http://chatbot.bonde.devel:5003',
+    chatbot: 'http://admin-canary.bonde.devel:5001/chatbot',
     redes: 'http://redes.bonde.devel:5004',
     // Essentials URLs
     crossStorage: 'http://cross-storage.bonde.devel',
@@ -14,8 +14,8 @@ export default {
     // Modules URLs
     accounts: 'https://accounts.staging.bonde.org/login',
     mobilization: 'https://app.staging.bonde.org/mobilizations',
-    settings: 'https://admin-canary.staging.bonde.org/community/settings',
-    chatbot: 'https://chatbot.staging.bonde.org',
+    settings: 'https://admin-canary.staging.bonde.org/community',
+    chatbot: 'https://admin-canary.staging.bonde.org/chatbot',
     redes: 'https://redes.staging.bonde.org',
     // Essentials URLs
     crossStorage: 'https://cross-storage.staging.bonde.org',
@@ -26,7 +26,7 @@ export default {
     accounts: 'https://accounts.bonde.org/login',
     mobilization: 'https://app.bonde.org/mobilizations',
     settings: 'https://admin-canary.bonde.org/community/settings',
-    chatbot: 'https://chatbot.bonde.org',
+    chatbot: 'https://admin-canary.bonde.org/chatbot',
     redes: 'https://redes.bonde.org',
     // Essentials URLs
     crossStorage: 'https://cross-storage.bonde.org',
