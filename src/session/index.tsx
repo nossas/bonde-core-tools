9df25beef57836f007ba320275cca7e4ff34e04f
Igor Santos <igr.exe@gmail.com>
2020-11-10 14:09:58 -0300

    fix(session): export gql

--- src/session/index.tsx
+++ src/session/index.tsx
@@ -1,4 +1,5 @@
 export { useQuery, useMutation } from '@apollo/react-hooks';
+export { default as gql } from 'graphql-tag';
 export { default as BondeSessionUI } from './components/BondeSessionUI';
 export { default as CommunityMenu } from './components/CommunityMenu';
 export { default as BondeSessionProvider, useSession } from './SessionProvider';
