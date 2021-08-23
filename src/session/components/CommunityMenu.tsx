a14355dd9265f39ef2e8da00881a4891125935e1
Igor Santos <igr.exe@gmail.com>
2021-08-23 18:22:27 -0300

    feat: add chakra + bonde-components

--- src/session/components/CommunityMenu.tsx
+++ src/session/components/CommunityMenu.tsx
@@ -1,82 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Button, Icon } from 'bonde-components';
-// import { Module } from '../../settings/types';
+import { IconButton, Icon, Stack } from 'bonde-components';
 import { useSession } from '../SessionProvider';
 import { Community } from '../types';
-
-interface MenuStyledProps {
-  inverted?: boolean;
-}
-
-const MenuStyled = styled.div<MenuStyledProps>`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-
-  button {
-    border: none;
-    padding: 0;
-
-    ${props =>
-      props.inverted &&
-      `
-      &.active {
-        ${Icon} {
-          &.stroke {
-            path {
-              stroke: #ee0099;
-            }
-          }
-          &.fill {
-            path {
-              fill: #ee0099;
-            }
-          }
-        }
-      }
-    `}
-  }
-
-  ${Icon} {
-    margin-left: 10px;
-  }
-`;
-
-MenuStyled.defaultProps = {
-  inverted: false,
-};
-
-interface MenuItemProps {
-  active?: boolean;
-  icon: any; // eslint-disable-line react/prop-types
-  title: string;
-  onClick: any; // eslint-disable-line react/prop-types
-  inverted?: boolean;
-}
-
-const MenuItem: React.FC<MenuItemProps> = ({
-  icon,
-  title,
-  onClick,
-  inverted,
-  active,
-}) => (
-  <Button
-    className={active ? 'active' : ''}
-    dark={!inverted}
-    light={inverted}
-    onClick={onClick}
-    title={title}
-  >
-    <Icon name={icon} size="small" />
-  </Button>
-);
-
-MenuItem.defaultProps = {
-  inverted: false,
-  active: false,
-};
 
 interface ItemsConfig {
   [index: string]: string[];
@@ -92,44 +17,39 @@ const items: ItemsConfig = {
 interface CommunityMenuProps {
   community: Community;
   inverted?: boolean;
+  size?: string | string[]
 }
 
-const CommunityMenu: React.FC<CommunityMenuProps> = ({
-  community,
-  inverted,
-}) => {
+const CommunityNavigation = ({ community, size }: CommunityMenuProps): React.ReactElement => {
   const { onChange, config } = useSession();
   const { modules } = community;
 
-  const click = (url: string) => async () => {
+  const handleClick = (url: string) => async () => {
     await onChange({ community, url });
   };
 
   return (
-    <MenuStyled inverted={inverted}>
+    <Stack direction="row" size={size}>
       {Object.keys(modules)
-        .filter((key: string) => !!modules[key])
-        .map((key: any) => {
-          const moduleHost: string = new URL('', config[key]).host;
-          const baseHost: string = new URL('', window.location.href).host;
+          .filter((key: string) => !!modules[key])
+          .map((key: any, index: number) => {
+            const moduleHost: string = new URL('', config[key]).host;
+            const baseHost: string = new URL('', window.location.href).host;
 
-          return (
-            <MenuItem
-              active={moduleHost === baseHost}
-              inverted={inverted}
-              key={key}
-              icon={items[key][0]}
-              title={items[key][1]}
-              onClick={click(config[key])}
-            />
-          );
-        })}
-    </MenuStyled>
+            return (
+              <IconButton
+                key={`community-navigation-${index}`}
+                variant="link"
+                title={items[key][1]}
+                icon={<Icon name={items[key][0]} />}
+                onClick={handleClick(config[key])}
+                active={moduleHost === baseHost}
+              />
+            );
+          })
+      }
+    </Stack>
   );
-};
+}
 
-CommunityMenu.defaultProps = {
-  inverted: false,
-};
-
-export default CommunityMenu;
+export default CommunityNavigation;
