55c9c7c06a921ae987e14f6ed2b2e2de881b2bc4
Igor Santos <igr.exe@gmail.com>
2021-08-31 20:13:36 -0300

    feat: change components used in UserDropdown

--- src/session/components/UserDropdown.tsx
+++ src/session/components/UserDropdown.tsx
@@ -1,4 +1,5 @@
 import {
+	Button,
 	Menu,
 	MenuButton,
 	MenuList,
@@ -7,40 +8,51 @@ import {
 	CloseIcon,
 	Text,
 	Stack,
-	Heading
+	Heading,
+	ArrowDownIcon,
+	ArrowUpIcon
 } from "bonde-components"
 import { useSession } from '../SessionProvider';
 
 import React from 'react'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const ChakraDropdown = () => {
+const UserDropdown = () => {
 	const { user, logout } = useSession();
 	const name = `${user.firstName} ${user.lastName}`;
 
 	const title = (
-		<Stack spacing={2}>
-			<Heading as="h3" size="md" textTransform="capitalize">{name}</Heading>
-			<Text fontSize="md">{user.email}</Text>
+		<Stack spacing={1}>
+			<Heading as="h3" size="md" fontWeight="extrabold" textTransform="capitalize">{name}</Heading>
+			<Text fontSize="md" color="gray.200">{user.email}</Text>
 		</Stack>
 	)
 	return (
-		<Menu>
-			<MenuButton aria-label="User Menu" color="white" textTransform="uppercase">
-				{name}
-			</MenuButton>
-			
-			<MenuList>
-				<MenuGroup title={title}>
-					<MenuItem onClick={logout} icon={<CloseIcon boxSize={3}/>}>
-						Logout
-					</MenuItem>
-				</MenuGroup>
-			</MenuList>
+		<Menu variant="dropdown">
+			{({ isOpen }: any) => (
+				<>
+					<MenuButton
+						aria-label="User Menu"
+						as={Button}
+						variant="dropdown"
+						color="white"
+						rightIcon={isOpen ? <ArrowUpIcon boxSize={3} /> : <ArrowDownIcon boxSize={3} />}
+					>
+						{name}
+					</MenuButton>
+					<MenuList>
+						<MenuGroup title={title}>
+							<MenuItem onClick={logout} icon={<CloseIcon boxSize={3}/>}>
+								Logout
+							</MenuItem>
+						</MenuGroup>
+					</MenuList>
+				</>
+			)}
 		</Menu>
 	)
 }
 
-export default ChakraDropdown;
+export default UserDropdown;
 
 
