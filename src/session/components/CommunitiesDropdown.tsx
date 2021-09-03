fb13f4535c4e12a22761bdd9ae02b8ed85f355e1
Igor Santos <igr.exe@gmail.com>
2021-09-03 17:31:54 -0300

    feat: use perform dropdown to communities

--- src/session/components/CommunitiesDropdown.tsx
+++ src/session/components/CommunitiesDropdown.tsx
@@ -1,64 +1,50 @@
-import React from 'react';
+import React from "react";
 import {
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  ArrowDownIcon,
-  ArrowUpIcon,
-  Image,
   Stack,
-  Heading,
-  Text
-} from "bonde-components"
-import { useSession } from '../SessionProvider';
-import CommunityMenu from './CommunityMenu';
+  Image,
+  Text,
+  PerformDropdown,
+  PerformDropdownList,
+  PerformDropdownItem,
+  PerformDropdownButton
+} from "bonde-components";
+import { useSession } from "../SessionProvider";
+import CommunityMenu from "./CommunityMenu";
 
 const CommunitiesDropdown = () => {
-  const { community, communities } = useSession();
-
+  const { community, communities, onChange } = useSession();
+  
   return (
     <Stack direction="row" spacing={2}>
-      <Menu>
-        {({ isOpen }: any) => (
-          <>
-            <MenuButton
-              aria-label="User Menu"
-              as={Button}
-              variant="dropdown"
-              color="white"
-              rightIcon={isOpen ? <ArrowUpIcon boxSize={3} /> : <ArrowDownIcon boxSize={3} />}
+      <PerformDropdown>
+        <PerformDropdownButton variant="link" color="white">
+          {community ? (
+            <Stack direction="row" spacing={4} alignItems="center">
+              <Image
+                boxSize={6}
+                rounded="50%"
+                src={community?.image || `https://via.placeholder.com/100?text=${community.name.substring(0, 1)}`}
+              />
+              <Text fontSize="sm" fontWeight="extrabold" textTransform="uppercase">{community?.name}</Text>
+            </Stack>
+          ) : "Selecione uma comunidade"}
+        </PerformDropdownButton>
+        <PerformDropdownList scroll={communities.length > 10}>
+          {communities.map((c: any) => (
+            <PerformDropdownItem
+              key={c.id}
+              onClick={() => {
+                onChange({ community: c });
+              }}
             >
-              {community?.name ? (
-                <Stack direction="row" spacing={4} alignItems="center">
-                  <Image
-                    src={community.image || `https://via.placeholder.com/100?text=${community.name.substring(0, 1)}`}
-                    boxSize={6}
-                    rounded="50%"
-                  />
-                  <Heading as="h6" size="xs">{community.name}</Heading>
-                </Stack>
-              ) : "Selecione uma comunidade"}
-            </MenuButton>
-
-            <MenuList maxH="300px" overflowY="scroll">
-              {communities.map((community) => (
-                <MenuItem key={community.id}>
-                  <Stack direction="row" spacing={4}>
-                    <Image
-                      src={community.image || `https://via.placeholder.com/100?text=${community.name.substring(0, 1)}`}
-                      boxSize={9}
-                      rounded="50%"
-                    />
-                    <Text>{community.name}</Text>
-                  </Stack>
-                </MenuItem>
-              ))}
-            </MenuList>
-          </>
-        )}
-      </Menu>
+              <Stack direction="row" spacing={4} p={4}>
+                <Image boxSize={8} rounded="50%" src={c.image || `https://via.placeholder.com/100?text=${c.name.substring(0, 1)}`} />
+                <Text>{c.name}</Text>
+              </Stack>
+            </PerformDropdownItem>
+          ))}
+        </PerformDropdownList>
+      </PerformDropdown>
       {community && <CommunityMenu community={community} inverted />}
     </Stack>
   )
