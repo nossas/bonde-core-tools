45ab5bb25cd31b4998c0d9b3ff90662d21f960e3
Igor Santos <igr.exe@gmail.com>
2021-12-06 18:29:46 -0300

    feat: export useLazyQuery

--- src/session/index.tsx
+++ src/session/index.tsx
@@ -1,4 +1,4 @@
-export { useQuery, useMutation, gql } from '@apollo/client';
+export { useQuery, useMutation, gql, useLazyQuery } from '@apollo/client';
 export { default as BondeSessionUI } from './components/BondeSessionUI';
 export { default as CommunityMenu } from './components/CommunityMenu';
 export { default as BondeSessionProvider, useSession } from './SessionProvider';
