7749015395262b3d3e6a3ded60a389b98237bc1e
Igor Santos <igr.exe@gmail.com>
2022-01-12 16:28:33 -0300

    feat: add js-cookie

--- src/session/graphql-client/index.ts
+++ src/session/graphql-client/index.ts
@@ -1,9 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import cookies from 'js-cookie';
 
 export default (uri: string, session: any = {}, policies: any = {}): any => {
+  console.log("cookies.session", cookies.get('session'));
   const apolloClientOpts: any = {
     uri,
     cache: new InMemoryCache(policies),
+    credentials: 'include',
     headers: {
       'client-name': 'bonde-core-tools [web]',
       'client-version': '1.0.0',
