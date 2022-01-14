51d5cc8b87ddc6a5dffae56ffa91f853b6b0e69a
Igor Santos <igr.exe@gmail.com>
2022-01-14 10:28:34 -0300

    chore: check cookies response

--- src/session/graphql-client/index.ts
+++ src/session/graphql-client/index.ts
@@ -1,21 +1,44 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, concat } from '@apollo/client';
 import cookies from 'js-cookie';
 
-export default (uri: string, session: any = {}, policies: any = {}): any => {
-  console.log("cookies.session", cookies.get('session'));
+export default (uri: string, _session: any = {}, policies: any = {}): any => {
   const apolloClientOpts: any = {
-    uri,
     cache: new InMemoryCache(policies),
     credentials: 'include',
+    request: (args1: any, args2: any) => {
+      console.log('request', { args1, args2 });
+    },
     headers: {
       'client-name': 'bonde-core-tools [web]',
       'client-version': '1.0.0',
     },
   };
 
-  if (session.token) {
-    apolloClientOpts.headers['authorization'] = `Bearer ${session.token}`;
-  }
 
-  return new ApolloClient(apolloClientOpts);
+  const httpLink = new HttpLink({
+    uri,
+    credentials: 'include'
+  })
+
+  const cookieSessionMiddleware = new ApolloLink((operation, forward) => {
+    console.log("cookies.session", cookies.get('session'));
+    console.log('cookieSessionMiddleware context', operation.getContext());
+    // add the authorization to the headers
+    // operation.setContext({
+    //   headers: {
+    //     authorization: localStorage.getItem('token') || null,
+    //   }
+    // });
+  
+    return forward(operation);
+  });
+
+  // if (session.token) {
+  //   apolloClientOpts.headers['authorization'] = `Bearer ${session.token}`;
+  // }
+
+  return new ApolloClient({
+    ...apolloClientOpts,
+    link: concat(cookieSessionMiddleware, httpLink)
+  });
 };
